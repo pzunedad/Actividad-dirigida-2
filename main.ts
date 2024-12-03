f32699d79c52e1434289e6916a14b39839b1200e
pzunedad <144534246+pzunedad@users.noreply.github.com>
2024-12-03 11:53:02 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -5,7 +5,7 @@ import { VuelosModel } from "./types.ts";
 import { startStandaloneServer } from '@apollo/server/standalone';
 import { resolvers } from "./resolvers.ts";
 
-const MONGO_URL = Deno.env.get("MONGO_URL");
+const MONGO_URL ="mongodb+srv://otheruser:123456aaabbb@clusterpedro.txa9b.mongodb.net/?retryWrites=true&w=majority&appName=ClusterPedro";
 if(!MONGO_URL){
     throw new Error("Please provide a MONGO_URL");
 }
